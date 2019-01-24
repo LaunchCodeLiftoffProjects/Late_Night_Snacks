838f3fdaeb7c306730ebe198671497e3b8779c24
Tyrone Garner II <tyroneegarner@gmail.com>
2019-01-24 17:31:58 -0600

    mvc files.

---
+++ Late_Night_Snacks/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Late_Night_Snacks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Late_Night_Snacks/Models/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Late_Night_Snacks.Models
+{
+    public class Menu
+    {
+        public string MenuItem { get; set; }
+        public int Price { get; set; }
+        public int ID { get; set; }
+       
+
+
+    }
+}
