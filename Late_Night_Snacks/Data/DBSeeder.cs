2ea32d5daa64d3c2bb98f341b504e10736c78dd1
Lauren Nicole <theofferingstone@gmail.com>
2019-01-31 19:26:08 -0600

    Added more data and fixed repeat entry issue

--- Late_Night_Snacks/Data/DBSeeder.cs
+++ Late_Night_Snacks/Data/DBSeeder.cs
@@ -10,20 +10,21 @@ namespace Late_Night_Snacks.Data
     {
         public static void SeedDB(MenuItemsDbContext context)
         {
+            context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
             context.MenuItems.AddRange(
                 new MenuItem()
                 {
                     Name = "HotDog",
                     Description = "Foot Long",
-                    Price = 1.99M,
+                    Price = 1.00M,
                     Quantity = 1,
                 },
                 new MenuItem()
                 {
                     Name = "Pepperoni Pizza",
                     Description = "Once Slice",
-                    Price = 2.99M,
+                    Price = 2.00M,
                     Quantity = 1,
                 },
                 new MenuItem()
@@ -31,6 +32,50 @@ namespace Late_Night_Snacks.Data
                     Name = "Hot Fudge Sunday", 
                     Description = "Vanilla", 
                     Price = 5.00M,
+                    Quantity = 1
+                },
+                new MenuItem()
+                {
+                    Name = "Chips",
+                    Description = "Salty",
+                    Price = 1.00M,
+                    Quantity = 1
+                }, new MenuItem()
+                {
+                    Name = "Popcorn",
+                    Description = "Kettle",
+                    Price = 6.00M,
+                    Quantity = 1
+                }, new MenuItem()
+                {
+                    Name = "Funnel Cake",
+                    Description = "Any Topping",
+                    Price = 3.00M,
+                    Quantity = 1
+                }, new MenuItem()
+                {
+                    Name = "Cheese Burger",
+                    Description = "Double Stacked",
+                    Price = 5.00M,
+                    Quantity = 1
+                }, new MenuItem()
+                {
+                    Name = "Cucumber",
+                    Description = "Crunchy",
+                    Price = 1.00M,
+                    Quantity = 1
+                },
+                new MenuItem()
+                {
+                    Name = "Strawberry Shortcake",
+                    Description = "With Whip",
+                    Price = 8.00M,
+                    Quantity = 1
+                }, new MenuItem()
+                {
+                    Name = "Steak and Eggs",
+                    Description = "With Hashbrowns",
+                    Price = 12.00M,
                     Quantity = 1
                 }
                 );
