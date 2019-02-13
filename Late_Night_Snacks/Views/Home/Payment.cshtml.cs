ef5e54d8f30e7f4fbd9eeb9f1fe9320d40add0aa
JennaFlo <jlflores@charter.net>
2019-02-12 19:19:00 -0600

    added the payment page page and activated the paypal image to open paypal page

---
+++ Late_Night_Snacks/Views/Home/Payment.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Late_Night_Snacks.Views.Home
+{
+    public class PaymentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
