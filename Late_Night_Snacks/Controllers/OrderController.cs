b6be5e2cbb28de5b7f57fd691bfa798175df5401
Jake <cuppajake@gmail.com>
2019-02-14 18:57:55 -0600

    Order Progress

--- Late_Night_Snacks/Controllers/OrderController.cs
+++ Late_Night_Snacks/Controllers/OrderController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Late_Night_Snacks.Data;
+using Late_Night_Snacks.Models;
+using Late_Night_Snacks.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -23,6 +25,26 @@ namespace Late_Night_Snacks.Controllers
         {
             return View();
         }
+
+        public IActionResult CreateOrder(OrderViewModel orderViewModel)
+        {
+            List<MenuItem> aList = new List<MenuItem>();
+
+            foreach (MenuItem menuItem in orderViewModel.MenuItems)
+            {
+                aList.Add(menuItem);
+            }
+
+            Order order = new Order()
+            {
+                MenuItems = aList,
+                CustomerName = orderViewModel.CustomerName,
+            };
+
+            return RedirectToAction("Index", order);
+
+        }
+
 
     }
 }
---
+++ Late_Night_Snacks/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Late_Night_Snacks.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public List<MenuItem> MenuItems { get; set; }
+    }
+}
